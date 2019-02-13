b7001e5360f1ad5d06911575efb35e7957689227
soufianeMHAIDRA <soufiane.mhaidra@outlook.com>
2019-02-13 20:01:16 +0100

    html runner

--- PersonTest.py
+++ PersonTest.py
@@ -1,5 +1,5 @@
 import unittest
-import xmlrunner
+import HtmlTestRunner
 
 # This is the class we want to test. So, we need to import it
 import Person as PersonClass
@@ -15,7 +15,7 @@ class Test(unittest.TestCase):
 
     # test case function to check the Person.set_name function
     def test_0_set_name(self):
-        print("Start set_name test\n")
+        # print("Start set_name test\n")
         """
         Any method which starts with ``test_`` will considered as a test case.
         """
@@ -30,34 +30,35 @@ class Test(unittest.TestCase):
             self.assertIsNotNone(user_id)  # null user id will fail the test
             # store the user id to the list
             self.user_id.append(user_id)
-        print("user_id length = ", len(self.user_id))
-        print(self.user_id)
-        print("user_name length = ", len(self.user_name))
-        print(self.user_name)
-        print("\nFinish set_name test\n")
+        # print("user_id length = ", len(self.user_id))
+        # print(self.user_id)
+        # print("user_name length = ", len(self.user_name))
+        # print(self.user_name)
+        # print("\nFinish set_name test\n")
 
     # test case function to check the Person.get_name function
     def test_1_get_name(self):
-        print("\nStart get_name test\n")
+        # print("\nStart get_name test\n")
         """
         Any method that starts with ``test_`` will be considered as a test case.
         """
         length = len(self.user_id)  # total number of stored user information
-        print("user_id length = ", length)
-        print("user_name length = ", len(self.user_name))
+        # print("user_id length = ", length)
+        # print("user_name length = ", len(self.user_name))
+        self.assertEqual(2,3)
         for i in range(6):
             # if i not exceed total length then verify the returned name
             if i < length:
                 # if the two name not matches it will fail the test case
                 self.assertEqual(self.user_name[i], self.person.get_name(self.user_id[i]))
             else:
-                print("Testing for get_name no user test")
+                # print("Testing for get_name no user test")
                 # if length exceeds then check the 'no such user' type message
                 self.assertEqual('There is no such user', self.person.get_name(i))
-        print("\nFinish get_name test\n")
+        # print("\nFinish get_name test\n")
 
 
 if __name__ == '__main__':
     # begin the unittest.main()
-    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='./testResults'))
+    unittest.main(testRunner=HtmlTestRunner.HTMLTestRunner(output='./'))
     
